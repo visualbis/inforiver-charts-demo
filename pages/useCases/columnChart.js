885031996108a312dfade02ae56141693b21bef7
akileswari <akileswarikumar@gmail.com>
2022-02-23 20:42:42 +0530

    build error fixed

--- pages/useCases/columnChart.js
+++ pages/useCases/columnChart.js
@@ -5,11 +5,12 @@ import dataRows from "../datasets/dualgroup";
 import { groupBy } from "../utils";
 
 const getColumnChart = () => {
-    toggleContainer(true);
-    const pageTitle = document.getElementById('page-title');
-    pageTitle.textContent = 'Column Chart';
-    const codeOptions = document.getElementById('code-options');
-    const optionsValue = hljs.highlight(`
+  toggleContainer(true);
+  const pageTitle = document.getElementById("page-title");
+  pageTitle.textContent = "Column Chart";
+  const codeOptions = document.getElementById("code-options");
+  const optionsValue = hljs.highlight(
+    `
         const options = {
             container: '<HTML Element>',
             chartOptions: {
@@ -23,31 +24,33 @@ const getColumnChart = () => {
                 dataRows: '<Datrows>',
             }
         }
-    `, { language: 'javascript' });
-    codeOptions.innerHTML = optionsValue.value;
-    const visualDOM = document.getElementById('visual-render-area');
-    const columnChart = new InforiverChart({
-        container: visualDOM,
-        chartOptions: {
-            chartType: 'columnChart'
-        },
-        datasource: {
-            metadata: {
-                row: "Country",
-                columns: ["Year", "Quarter"],
-                measures: ["Sales", "Profit"],
-            },
-            dataRows,
-        },
-        feature: {
-            toolbar: {
-                enabled: true,
-                exportAsPDF: true,
-                sidePanelView: true
-            }
-        }
-    });
-    columnChart.render();
-}
+    `,
+    { language: "javascript" }
+  );
+  codeOptions.innerHTML = optionsValue.value;
+  const visualDOM = document.getElementById("visual-render-area");
+  const columnChart = new InforiverChart({
+    container: visualDOM,
+    chartOptions: {
+      chartType: "columnChart",
+    },
+    datasource: {
+      metadata: {
+        row: "Country",
+        columns: ["Year", "Quarter"],
+        measures: ["AC"],
+      },
+      dataRows,
+    },
+    feature: {
+      toolbar: {
+        enabled: true,
+        exportAsPDF: true,
+        sidePanelView: true,
+      },
+    },
+  });
+  columnChart.render();
+};
 
 export default getColumnChart;
--- pages/useCases/columnChartHorizontal.js
+++ pages/useCases/columnChartHorizontal.js
@@ -4,11 +4,12 @@ import { toggleContainer } from "../../assets/scripts/router";
 import dataRows from "../datasets/complex";
 
 const getColumnChartHorizontal = () => {
-    toggleContainer(true);
-    const pageTitle = document.getElementById('page-title');
-    pageTitle.textContent = 'Column Chart (Horizontal)';
-    const codeOptions = document.getElementById('code-options');
-    const optionsValue = hljs.highlight(`
+  toggleContainer(true);
+  const pageTitle = document.getElementById("page-title");
+  pageTitle.textContent = "Column Chart (Horizontal)";
+  const codeOptions = document.getElementById("code-options");
+  const optionsValue = hljs.highlight(
+    `
         const options = {
             container: '<HTML Element>',
             chartOptions: {
@@ -23,24 +24,26 @@ const getColumnChartHorizontal = () => {
                 dataRows: '<Datrows>',
             }
         }
-    `, { language: 'javascript' });
-    codeOptions.innerHTML = optionsValue.value;
-    const visualDOM = document.getElementById('visual-render-area');
-    const columnChart = new InforiverChart({
-        container: visualDOM,
-        chartOptions: {
-            chartType: 'columnChart',
-            orientation: 'horizontal'
-        },
-        datasource: {
-            metadata: {
-                row: "Country",
-                measures: ["Sales"],
-            },
-            dataRows,
-        }
-    });
-    columnChart.render();
-}
+    `,
+    { language: "javascript" }
+  );
+  codeOptions.innerHTML = optionsValue.value;
+  const visualDOM = document.getElementById("visual-render-area");
+  const columnChart = new InforiverChart({
+    container: visualDOM,
+    chartOptions: {
+      chartType: "columnChart",
+      orientation: "horizontal",
+    },
+    datasource: {
+      metadata: {
+        row: "Country",
+        measures: ["AC"],
+      },
+      dataRows,
+    },
+  });
+  columnChart.render();
+};
 
 export default getColumnChartHorizontal;
